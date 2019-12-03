10deb8b634782e83ee804367d4021f1bb2bc2834
Oliver Booth <me@olivr.me>
2019-12-03 21:19:58 +0000

    🚨 Add tests for action building and parsing

---
+++ VirtualParadise.Tests/src/ActionParserTest.cs
@@ -0,0 +1,92 @@
+﻿namespace VirtualParadise.Tests
+{
+    #region Using Directives
+
+    using System.Linq;
+    using API;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Scene.Serialization;
+    using Scene.Serialization.Commands;
+    using Scene.Serialization.Triggers;
+
+    #endregion
+
+    [TestClass]
+    public class ActionParserTest
+    {
+        #region Methods
+
+        [TestMethod]
+        public void TestActivateTexture()
+        {
+            Action      action       = Action.Parse("activate texture stone1 global");
+            TriggerBase firstTrigger = action.Triggers.First();
+            CommandBase firstCommand = firstTrigger.Commands.First();
+
+            Assert.IsTrue(firstTrigger is ActivateTrigger);
+            Assert.IsTrue(firstCommand is TextureCommand);
+
+            TextureCommand textureCommand = firstCommand as TextureCommand;
+            Assert.IsNotNull(textureCommand);
+            Assert.AreEqual("stone1", textureCommand.Texture);
+            Assert.IsTrue(textureCommand.IsGlobal);
+        }
+
+        [TestMethod]
+        public void TestCreateSign()
+        {
+            Action      action       = Action.Parse("create sign \"Hello World\" bcolor=blue color=red");
+            TriggerBase firstTrigger = action.Triggers.First();
+            CommandBase firstCommand = firstTrigger.Commands.First();
+
+            Assert.IsTrue(firstTrigger is CreateTrigger);
+            Assert.IsTrue(firstCommand is SignCommand);
+
+            SignCommand signCommand = firstCommand as SignCommand;
+            Assert.IsNotNull(signCommand);
+            Assert.AreEqual("Hello World", signCommand.Text);
+            Assert.AreEqual(Color.Blue,    signCommand.BackColor);
+            Assert.AreEqual(Color.Red,     signCommand.ForeColor);
+            Assert.IsFalse(signCommand.IsGlobal);
+        }
+
+        [TestMethod]
+        public void TestMultipleCreateCommands()
+        {
+            Action      action        = Action.Parse("create name foo, color red");
+            TriggerBase firstTrigger  = action.Triggers.First();
+            CommandBase firstCommand  = firstTrigger.Commands.First();
+            CommandBase secondCommand = firstTrigger.Commands.ElementAt(1);
+
+            Assert.IsTrue(firstTrigger is CreateTrigger);
+            Assert.IsTrue(firstCommand is NameCommand);
+            Assert.IsTrue(secondCommand is ColorCommand);
+
+            NameCommand  nameCommand  = firstCommand as NameCommand;
+            ColorCommand colorCommand = secondCommand as ColorCommand;
+
+            Assert.IsNotNull(nameCommand);
+            Assert.AreEqual("foo",     nameCommand.Name);
+            Assert.AreEqual(Color.Red, colorCommand.Color);
+            Assert.IsFalse(colorCommand.Tint);
+        }
+
+        [TestMethod]
+        public void TestActionBuilderToString()
+        {
+            ActionBuilder builder = new ActionBuilder();
+            builder.AddTrigger(new CreateTrigger())
+                   .AddCommand(new NameCommand {Name = "foo"})
+                   .AddTrigger(new ActivateTrigger())
+                   .AddCommand(new DiffuseCommand {Intensity = 1.0, TargetName = "foo"});
+
+            Action action = builder.Build();
+            Assert.AreEqual(
+                "create name foo; activate diffuse 1 name=foo",
+                action.ToString(ActionFormat.None),
+                false);
+        }
+
+        #endregion
+    }
+}
