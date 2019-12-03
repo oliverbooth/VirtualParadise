9c8a6f1187341adae2285550d87d5d60e0d0e01d
Oliver Booth <me@olivr.me>
2019-12-03 15:00:15 +0000

    ✨ Add VP coordinate parser

---
+++ VirtualParadise/src/API/CoordinateParser.cs
@@ -0,0 +1,59 @@
+﻿namespace VirtualParadise.API
+{
+    #region Using Directives
+
+    using System;
+    using System.Text.RegularExpressions;
+
+    #endregion
+
+    /// <summary>
+    /// Represents a class which can translate a coordinate string to a <see cref="Coordinates"/>
+    /// instance.
+    /// </summary>
+    internal static class CoordinateParser
+    {
+        /// <summary>
+        /// Parses a coordinate string.
+        /// </summary>
+        /// <param name="coordinates">The coordinates to parse.</param>
+        /// <returns>Returns an instance of <see cref="Coordinates"/>.</returns>
+        public static Coordinates ParseCoordinates(string coordinates)
+        {
+            const string pattern =
+                @"(?:([a-z]+) *)?(?: *(\+)?(-?\d+(?:\.\d+)?)([ns])? +(\+)?(-?\d+(?:\.\d+)?)([we])?( +(\+)?(-?\d+(?:\.\d+)?)a)?( +(\+)?(-?\d+(?:\.\d+)?))?)?";
+
+            Regex regex    = new Regex(pattern, RegexOptions.IgnoreCase);
+            Match match    = regex.Match(coordinates);
+            bool  relative = match.Groups[2].Success || match.Groups[5].Success;
+
+            string world     = match.Groups[1].Success ? match.Groups[1].Value : String.Empty;
+            double z         = match.Groups[3].Success ? Convert.ToDouble(match.Groups[3].Value) : 0.0;
+            double x         = match.Groups[6].Success ? Convert.ToDouble(match.Groups[6].Value) : 0.0;
+            double y         = match.Groups[10].Success ? Convert.ToDouble(match.Groups[10].Value) : 0.0;
+            double direction = match.Groups[13].Success ? Convert.ToDouble(match.Groups[13].Value) : 0.0;
+
+            if (match.Groups[4].Success &&
+                match.Groups[4].Value.Equals("S", StringComparison.InvariantCultureIgnoreCase))
+            {
+                z = -z;
+            }
+
+            if (match.Groups[7].Success &&
+                match.Groups[7].Value.Equals("E", StringComparison.InvariantCultureIgnoreCase))
+            {
+                x = -x;
+            }
+
+            return new Coordinates
+            {
+                Z          = z,
+                X          = x,
+                Y          = y,
+                Direction  = direction,
+                World      = world,
+                IsRelative = relative
+            };
+        }
+    }
+}
---
+++ VirtualParadise/src/API/Coordinates.cs
@@ -0,0 +1,143 @@
+﻿namespace VirtualParadise.API
+{
+    #region Using Directives
+
+    using System;
+    using System.Text;
+
+    #endregion
+
+    /// <summary>
+    /// Represents a struct which contains Virtual Paradise coordinates.
+    /// </summary>
+    public struct Coordinates : IEquatable<Coordinates>
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the direction.
+        /// </summary>
+        public double Direction { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance represents relative coordinates.
+        /// </summary>
+        public bool IsRelative { get; set; }
+
+        /// <summary>
+        /// Gets or sets the world.
+        /// </summary>
+        public string World { get; set; }
+
+        /// <summary>
+        /// Gets or sets the X coordinate.
+        /// </summary>
+        public double X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Y coordinate.
+        /// </summary>
+        public double Y { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Z coordinate.
+        /// </summary>
+        public double Z { get; set; }
+
+        #endregion
+
+        #region Operators
+
+        public static bool operator ==(Coordinates left, Coordinates right) =>
+            left.Equals(right);
+
+        public static bool operator !=(Coordinates left, Coordinates right) =>
+            !(left == right);
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Parses a coordinate string.
+        /// </summary>
+        /// <param name="coordinates">The coordinates to parse.</param>
+        /// <returns>Returns an instance of <see cref="Coordinates"/>.</returns>
+        public static Coordinates Parse(string coordinates)
+        {
+            return CoordinateParser.ParseCoordinates(coordinates);
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            if (!String.IsNullOrWhiteSpace(this.World))
+            {
+                builder.Append(this.World)
+                       .Append(' ');
+            }
+
+            if (this.IsRelative)
+            {
+                builder.Append(this.Z >= 0.0 ? "+" : "")
+                       .Append(this.Z)
+                       .Append(' ')
+                       .Append(this.X >= 0.0 ? "+" : "")
+                       .Append(this.X)
+                       .Append(' ')
+                       .Append(this.Y >= 0.0 ? "+" : "")
+                       .Append(this.Y)
+                       .Append('a')
+                       .Append(' ')
+                       .Append(this.Direction >= 0.0 ? "+" : "")
+                       .Append(this.Direction);
+            }
+            else
+            {
+                builder.Append(Math.Abs(this.Z))
+                       .Append(this.Z >= 0.0 ? 'n' : 's')
+                       .Append(' ')
+                       .Append(Math.Abs(this.X))
+                       .Append(this.X >= 0.0 ? 'w' : 'e')
+                       .Append(' ')
+                       .Append(this.Y)
+                       .Append('a')
+                       .Append(' ')
+                       .Append(this.Direction);
+            }
+
+
+            return builder.ToString();
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            return obj is Coordinates other && this.Equals(other);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = this.Direction.GetHashCode();
+                hashCode = (hashCode * 397) ^ this.X.GetHashCode();
+                hashCode = (hashCode * 397) ^ this.Y.GetHashCode();
+                hashCode = (hashCode * 397) ^ this.Z.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        /// <inheritdoc />
+        public bool Equals(Coordinates other) =>
+            this.Direction.Equals(other.Direction) &&
+            this.X.Equals(other.X)                 &&
+            this.Y.Equals(other.Y)                 &&
+            this.Z.Equals(other.Z);
+
+        #endregion
+    }
+}
