101cd7574ec9fd5e2423c9ce2da743c5c11f1a2c
Oliver Booth <me@olivr.me>
2019-12-03 13:57:30 +0000

    📜 Place predefined values in a partial file

--- VirtualParadise/src/API/Color.cs
+++ VirtualParadise/src/API/Color.cs
@@ -14,7 +14,7 @@
     /// <summary>
     /// Represents a color.
     /// </summary>
-    public struct Color : IEquatable<Color>
+    public partial struct Color : IEquatable<Color>
     {
         #region Fields
 
@@ -51,72 +51,6 @@
             this.G = g;
             this.B = b;
         }
-
-        #endregion
-
-        #region Predefined Values
-
-        public static Color Aquamarine => new Color(0x70, 0xDB, 0x93);
-
-        public static Color Black => new Color(0x00, 0x00, 0x00);
-
-        public static Color Blue => new Color(0x00, 0x00, 0xFF);
-
-        public static Color Brass => new Color(0xB5, 0xA6, 0x42);
-
-        public static Color Bronze => new Color(0x8C, 0x78, 0x53);
-
-        public static Color Copper => new Color(0xB8, 0x73, 0x33);
-
-        public static Color Cyan => new Color(0x00, 0xFF, 0xFF);
-
-        public static Color DarkGrey => new Color(0x30, 0x30, 0x30);
-
-        public static Color ForestGreen => new Color(0x23, 0x8E, 0x23);
-
-        public static Color Gold => new Color(0xCD, 0x7F, 0x32);
-
-        public static Color Green => new Color(0x00, 0xFF, 0x00);
-
-        public static Color Grey => new Color(0x70, 0x70, 0x70);
-
-        public static Color LightGrey => new Color(0xC0, 0xC0, 0xC0);
-
-        public static Color Magenta => new Color(0xFF, 0x00, 0xFF);
-
-        public static Color Maroon => new Color(0x8E, 0x23, 0x6B);
-
-        public static Color NavyBlue => new Color(0x23, 0x23, 0x8E);
-
-        public static Color Orange => new Color(0xFF, 0x7F, 0x00);
-
-        public static Color OrangeRed => new Color(0xFF, 0x24, 0x00);
-
-        public static Color Orchid => new Color(0xDB, 0x70, 0xDB);
-
-        public static Color Pink => new Color(0xFF, 0x6E, 0xC7);
-
-        public static Color Red => new Color(0xFF, 0x00, 0x00);
-
-        public static Color Salmon => new Color(0x6F, 0x42, 0x42);
-
-        public static Color Scarlet => new Color(0x8C, 0x17, 0x17);
-
-        public static Color Silver => new Color(0xE6, 0xE8, 0xFA);
-
-        public static Color SkyBlue => new Color(0x32, 0x99, 0xCC);
-
-        public static Color Tan => new Color(0xDB, 0x93, 0x70);
-
-        public static Color Teal => new Color(0x00, 0x70, 0x70);
-
-        public static Color Turquoise => new Color(0xAD, 0xEA, 0xEA);
-
-        public static Color Violet => new Color(0x4F, 0x2F, 0x4F);
-
-        public static Color White => new Color(0xFF, 0xFF, 0xFF);
-
-        public static Color Yellow => new Color(0xFF, 0xFF, 0x00);
 
         #endregion
 
