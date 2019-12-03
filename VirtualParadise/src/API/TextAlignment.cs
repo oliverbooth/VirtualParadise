ba8ed20ca7a125389e8ccec60edf011ac3591767
Oliver Booth <me@olivr.me>
2019-12-03 14:00:00 +0000

    ✨ Add TextAlignment num

---
+++ VirtualParadise/src/API/TextAlignment.cs
@@ -0,0 +1,32 @@
+﻿namespace VirtualParadise.API
+{
+    #region Using Directives
+
+    using System.ComponentModel;
+
+    #endregion
+
+    /// <summary>
+    /// An enumeration of text alignments.
+    /// </summary>
+    public enum TextAlignment
+    {
+        /// <summary>
+        /// Center text alignment.
+        /// </summary>
+        [Description("Center text alignment.")]
+        Center,
+
+        /// <summary>
+        /// Left text alignment.
+        /// </summary>
+        [Description("Left text alignment.")]
+        Left,
+
+        /// <summary>
+        /// Right text alignment.
+        /// </summary>
+        [Description("Right text alignment.")]
+        Right
+    }
+}
