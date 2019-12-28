dd0405d54dda0e0a5f67479acf4210799c1a21b6
Oliver Booth <me@olivr.me>
2019-12-28 13:30:20 +0000

    ✨ Add ObjectType enum

---
+++ VirtualParadise/src/Scene/ObjectType.cs
@@ -0,0 +1,40 @@
+﻿namespace VirtualParadise.Scene
+{
+    #region Using Directives
+
+    using System;
+    using System.ComponentModel;
+
+    #endregion
+
+    /// <summary>
+    /// An enumeration of object types.
+    /// </summary>
+    public enum ObjectType
+    {
+        /// <summary>
+        /// 3D object.
+        /// </summary>
+        [Description("3D object")]
+        Object3D,
+
+        /// <summary>
+        /// Cloth object.
+        /// </summary>
+        [Description("Cloth object")]
+        [Obsolete]
+        Cloth,
+
+        /// <summary>
+        /// Particle emitter.
+        /// </summary>
+        [Description("Particle emitter")]
+        Particle,
+
+        /// <summary>
+        /// Path.
+        /// </summary>
+        [Description("Path")]
+        Path
+    }
+}
