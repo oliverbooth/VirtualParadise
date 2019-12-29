542881f8748e9e4f3a596c1037dc2b25b59a3588
Oliver Booth <me@olivr.me>
2019-12-29 13:57:42 +0000

    ✨ Add ToString override for ColorEnum

---
+++ VirtualParadise/src/Scene/Extensions/ColorEnumExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace VirtualParadise.Scene.Extensions
+{
+    #region Using Directives
+
+    using API;
+
+    #endregion
+
+    /// <summary>
+    /// Extension methods for <see cref="ColorEnum"/>.
+    /// </summary>
+    public static class ColorEnumExtensions
+    {
+        /// <summary>
+        /// Overrides <see cref="ColorEnum.ToString()"/> to call <see cref="Color.ToString()"/>.
+        /// </summary>
+        /// <param name="c">The color.</param>
+        /// <returns>Returns a string representation of the color.</returns>
+        public static string ToString(this ColorEnum c)
+        {
+            return ((Color) c).ToString();
+        }
+    }
+}
