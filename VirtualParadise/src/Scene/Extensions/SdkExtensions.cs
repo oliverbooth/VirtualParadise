6af03bb756382153e5f0063872b8f3e1a8f174b1
Oliver Booth <me@olivr.me>
2020-01-04 15:23:52 +0000

    ✨ Add click handler for triggers or object names

---
+++ VirtualParadise/src/Scene/Extensions/SdkExtensions.cs
@@ -0,0 +1,43 @@
+﻿namespace VirtualParadise.Scene.Extensions
+{
+    #region Using Directives
+
+    using System;
+    using Serialization.Commands;
+    using VpNet;
+    using Action = Serialization.Action;
+    using Callback = System.Action<object, VpNet.ObjectClickArgsT<VpNet.Avatar, VpNet.VpObject>>;
+
+    #endregion
+
+    public static class SdkExtensions
+    {
+        public static void AddObjectClickHandler(this Instance instance, string objectName, Callback callback)
+        {
+            instance.OnObjectClick += (sender, args) => {
+                VpObject obj    = args.VpObject;
+                Action   action = Action.Parse(obj.Action);
+                string   name   = action.Create?.GetCommandOfType<NameCommand>()?.Name ?? String.Empty;
+
+                if (name.Equals(objectName)) {
+                    callback(sender, args);
+                }
+            };
+        }
+
+        public static void AddObjectClickHandler<T>(this Instance                                         instance,
+                                                    Action<object, ObjectClickArgsT<Avatar, VpObject>, T> callback)
+            where T : Command
+        {
+            instance.OnObjectClick += (sender, args) => {
+                VpObject obj     = args.VpObject;
+                Action   action  = Action.Parse(obj.Action);
+                T        command = action.Activate?.GetCommandOfType<T>();
+
+                if (!(command is null)) {
+                    callback(sender, args, command);
+                }
+            };
+        }
+    }
+}
