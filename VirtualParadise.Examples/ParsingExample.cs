636033d39b0abaf6aacdd6c948440f495d9c5fd0
Oliver Booth <me@olivr.me>
2020-01-04 15:31:58 +0000

    🧹 Clean up examples

---
+++ VirtualParadise.Examples/ParsingExample.cs
@@ -0,0 +1,73 @@
+﻿namespace VirtualParadise.Examples
+{
+    #region Using Directives
+
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using Scene.Serialization.Commands;
+    using Scene.Serialization.Triggers;
+    using Action = Scene.Serialization.Action;
+
+    #endregion
+
+    public static class ParsingExample
+    {
+        public static void RunExample()
+        {
+            const string input = "create scale 0.35, visible off, color tint red, ambient 1, diffuse 1, name foobar;" +
+                                 "activate astart myAnim global, move 0 -0.01 time=0.1 smooth global, visible on";
+
+            Action action = Action.Parse(input);
+            Console.WriteLine($"Input  = {input}");
+            Console.WriteLine($"Parsed = {action}\n");
+
+            Console.WriteLine("Parsed action analysis:");
+            OutputVerboseAction(action);
+        }
+
+        /// <summary>
+        /// Output all commands and triggers
+        /// </summary>
+        /// <param name="action">The action.</param>
+        private static void OutputVerboseAction(Action action)
+        {
+            Console.WriteLine($"Trigger count = {action.Triggers.Count()}");
+            foreach (Trigger trigger in action.Triggers) {
+                string triggerName = trigger.TriggerName.ToUpperInvariant();
+                Console.WriteLine($"  {triggerName} command count: {trigger.Commands.Count()}");
+                foreach (Command command in trigger.Commands) {
+                    string[] ignoredProperties = new[] {
+                        nameof(Command.CommandName),
+                        nameof(Command.Arguments),
+                        nameof(Command.Properties),
+                        nameof(Command.Flags)
+                    };
+
+                    IEnumerable<PropertyInfo> properties = command.GetType().GetProperties();
+                    Console.WriteLine($"      {nameof(Command.CommandName)} = {command.CommandName}");
+
+                    foreach (PropertyInfo property in properties) {
+                        if (ignoredProperties.Contains(property.Name, StringComparer.InvariantCultureIgnoreCase)) {
+                            // don't output properties we want to ignore
+                            continue;
+                        }
+
+                        object value = property.GetValue(command, null);
+
+                        if ((value?.GetType().IsArray ?? false) && value is IEnumerable enumerable) {
+                            // join arrays
+                            value = String.Join(", ", enumerable.Cast<object>());
+                        }
+
+                        Console.WriteLine($"      {property.Name} = {value}");
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
--- VirtualParadise.Examples/Program.cs
+++ VirtualParadise.Examples/Program.cs
@@ -1,67 +1,25 @@
 ﻿namespace VirtualParadise.Examples
 {
+    #region Using Directives
+
     using System;
-    using System.Collections;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
-    using System.Threading.Tasks;
-    using Scene.Serialization.Commands;
-    using Scene.Serialization.Triggers;
-    using Action = Scene.Serialization.Action;
 
-    internal class Program
+    #endregion
+
+    internal static class Program
     {
-        private static async Task Main(string[] args)
+        private static void Main()
         {
-            const string input =
-                "create scale 0.35, visible off, color tint red, ambient 1, diffuse 1, name foobar;activate astart myanim global, move 0 -0.01 time=0.1 smooth global, visible on";
+            Console.WriteLine("Fluent API example:");
+            FluentExample.RunExample();
 
-            Action action = await Action.ParseAsync(input);
-            Console.WriteLine($"Input  = {input}");
-            Console.WriteLine($"Parsed = {action}\n");
+            Console.WriteLine("\nSequencing example:");
+            SequencingExample.RunExample();
 
-            OutputVerboseAction(action);
+            Console.WriteLine("\nParsing example:");
+            ParsingExample.RunExample();
 
             Console.ReadLine();
-        }
-
-        private static void OutputVerboseAction(Action action)
-        {
-            Console.WriteLine($"Trigger count = {action.Triggers.Count()}");
-            foreach (TriggerBase trigger in action.Triggers) {
-                string triggerName = trigger.TriggerName.ToUpperInvariant();
-                Console.WriteLine($"  {triggerName} command count: {trigger.Commands.Count()}");
-                foreach (CommandBase command in trigger.Commands) {
-                    string[] ignoredProperties = new[] {
-                        nameof(CommandBase.CommandName),
-                        nameof(CommandBase.Arguments),
-                        nameof(CommandBase.Properties),
-                        nameof(CommandBase.Flags)
-                    };
-
-                    IEnumerable<PropertyInfo> properties = command.GetType().GetProperties();
-                    Console.WriteLine($"      {nameof(CommandBase.CommandName)} = {command.CommandName}");
-
-                    foreach (PropertyInfo property in properties) {
-                        if (ignoredProperties.Contains(property.Name, StringComparer.InvariantCultureIgnoreCase)) {
-                            // don't output properties we want to ignore
-                            continue;
-                        }
-
-                        object value = property.GetValue(command, null);
-
-                        if ((value?.GetType().IsArray ?? false) && value is IEnumerable enumerable) {
-                            // join arrays
-                            value = String.Join(", ", enumerable.Cast<object>());
-                        }
-
-                        Console.WriteLine($"      {property.Name} = {value}");
-                    }
-
-                    Console.WriteLine();
-                }
-            }
         }
     }
 }
